1d791691968599243ea17bc8e1e9c3045c8179c9
banelmikhael <banel.mikhael.bm@gmail.com>
2022-10-19 19:45:39 +0300

    assigment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  
+    document.getElementById("create-task-form").addEventListener("submit" , function (e){
+      e.preventDefault()
+      addToDo(e.target.new_toDo.value)
+      
+    })
+
+
+    function addToDo(task){
+      let btn = document.createElement("button");
+      let new_task = document.createElement("li")
+      btn.innerHTML= "X";
+      
+      new_task.innerHTML = `${task} `
+new_task.appendChild(btn)
+      document.getElementById("tasks").appendChild(new_task);
+      btn.addEventListener("click" , function(){
+        new_task.remove();
+      })
+    }
+  }
